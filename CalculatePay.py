f319806fcf87b86174d835a69c84af6848e1b10f
Brandon Gealy <brandon.gealy@gmail.com>
2017-12-02 13:22:06 -0800

    Add files via upload

---
+++ CalculatePay.py
@@ -0,0 +1,9 @@
+# calculate a person's pay
+
+hours = float(input('Enter the number of hours worked: '))
+wage = float(input('Enter your wage per hour: '))
+pay = float(hours) * float(wage)
+
+print(pay)
+
+quit()
